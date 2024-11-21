a49a7b7660257cc0694410fc4931366a0d9599e4
srisanthe9 <srisanth.e9@gmail.com>
2024-11-21 21:32:45 +0530

    y first python program

---
+++ space.py
@@ -0,0 +1,13 @@
+if 5 > 2:
+    print("hello")
+    print("world")
+    x=5
+    y="hi"
+    #this is a comment
+    """hi everyone i am srisanth studying at ngit 
+    """
+    print(x,y)
+    x=5
+    y="candy"
+    print(type(x))
+    print(type(y))
